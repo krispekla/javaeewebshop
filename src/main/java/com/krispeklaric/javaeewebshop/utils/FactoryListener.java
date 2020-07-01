b71b630ac170dd387059952ebb89b7f325b1a8e8
Kris Peklaric <peklarick@gmail.com>
2020-07-01 09:11:13 +0200

    init

---
+++ src/main/java/com/krispeklaric/javaeewebshop/utils/FactoryListener.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.krispeklaric.javaeewebshop.utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Kris
+ */
+public class FactoryListener implements ServletContextListener {
+
+    static EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("com.krispeklaric_javaeewebshop_war_1.0PU");
+        context.setAttribute("emf", entityManagerFactory);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        entityManagerFactory.close();
+    }
+}
---
+++ target/generated-sources/annotations/com/krispeklaric/javaeewebshop/models/Test_.java
@@ -0,0 +1,13 @@
+package com.krispeklaric.javaeewebshop.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-07-01T09:06:00")
+@StaticMetamodel(Test.class)
+public class Test_ { 
+
+    public static volatile SingularAttribute<Test, Long> id;
+
+}
