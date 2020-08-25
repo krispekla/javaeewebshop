069426c894fd9499b708acb1dae2f3d24c0b0591
Kris Peklaric <peklarick@gmail.com>
2020-08-25 08:46:01 +0200

    update

--- target/generated-sources/annotations/com/krispeklaric/javaeewebshop/models/Category_.java
+++ target/generated-sources/annotations/com/krispeklaric/javaeewebshop/models/Category_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-22T11:45:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-22T11:54:23")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
